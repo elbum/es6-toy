53f122c87e16ebada5e8b681d63fba3d0c166047
elbum <elbum@hanmail.net>
2020-03-19 18:45:14 +0900

    last

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import blog from "./main";
+
+console.log("start project by webpack");
+
+const myblog = new blog();
+
+const myfun = () => {
+  let name = "crong12";
+  console.log("my names is", name);
+};
+
+myfun();
